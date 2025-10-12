98af2fd99f3f9259ee3848ce8d87ccc580c9ae91
AlterionX <ben.xu.cs@gmail.com>
2025-10-11 22:10:09 -0700

    Add async support for db

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,11 +1,27 @@
-use diesel::{Connection, PgConnection};
+use diesel::{Connection, ConnectionError, PgConnection};
+use diesel_async::{AsyncConnection, AsyncPgConnection};
+
+treeerror::treeerror! {
+    #[derive(Debug)]
+    DbError {
+        Connection(ConnectionError),
+        Query(diesel::result::Error),
+    },
+}
+
+pub type DbResult<T> = Result<T, DbError>;
 
 pub trait Connector {
-    fn connect(&self) -> PgConnection;
+    fn connect(&self) -> Result<PgConnection, ConnectionError>;
+    fn async_connect(&self) -> impl Future<Output = Result<AsyncPgConnection, ConnectionError>> + Send;
 }
 
 impl Connector for &super::DatabaseConfiguration {
-    fn connect(&self) -> PgConnection {
-        diesel::pg::PgConnection::establish(self.url.as_str()).expect("DB to connect")
+    fn connect(&self) -> Result<PgConnection, ConnectionError> {
+        PgConnection::establish(self.url.as_str())
+    }
+
+    async fn async_connect(&self) -> Result<AsyncPgConnection, ConnectionError> {
+        AsyncPgConnection::establish(self.url.as_str()).await
     }
 }
